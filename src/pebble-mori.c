f74e9b0479c5770d94ac7d50f58d75f35e6ca8e3
Nate Murray <nate@natemurray.com>
2013-04-19 20:34:26 -0700

    make a watch face

--- src/pebble-mori.c
+++ src/pebble-mori.c
@@ -5,10 +5,10 @@
 
 #define MY_UUID { 0x6D, 0x57, 0xBE, 0xFA, 0x8B, 0x81, 0x4B, 0xF4, 0x81, 0xE3, 0xAB, 0x14, 0x20, 0x70, 0xAB, 0xCD }
 PBL_APP_INFO(MY_UUID,
-             "Template App", "Your Company",
+             "Memento Mori", "Little Hiccup",
              1, 0, /* App version */
              DEFAULT_MENU_ICON,
-             APP_INFO_STANDARD_APP);
+             APP_INFO_WATCH_FACE);
 
 Window window;
 
